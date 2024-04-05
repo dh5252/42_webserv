726cb6ddc322cec86d4345829fc4ea7ff0bf8a86
vivivim <130360809+vivivim@users.noreply.github.com>
2024-04-05 21:18:51 +0900

    그냥 조금 수정

--- srcs/request/request.cpp
+++ srcs/request/request.cpp
@@ -33,15 +33,9 @@ void	request::parseStartLine(std::string	startLine)
 {	
 	std::stringstream	ss(startLine);
 
-	for (int i = 0; i < 3; ++i)
-	{
-		if (i == 0)
-			ss >> this->method; //is_body 설정을 내가 해야 되면 여기서
-		if (i == 1)
-			ss >> this->path;
-		if (i == 2)
-			ss >> this->version;
-	}
+	ss >> this->method >> this->path >> this->version;
+	if (this->method != "GET" && this->method != "POST" && this->method != "DELETE")
+		error;
 	//예외?
 }
 
@@ -52,12 +46,23 @@ void	request::parseHeaders(std::string raw)
 	std::string			key;
 	size_t				pos;
 
-	while (std::getline(is, header))
+	//빈줄이 나오면 헤더 끝
+	while (std::getline(is, header) && header != '\n')
 	{
-		if ((pos = header.find(": ")) != std::string::npos)
-		{
+		//여기서 콜론 뒤 공백은 선택적 공백
+		if (pos = header.find(':') != std::string::npos)
+		{	
 			key = header.substr(0, pos);
-			this->map[key] = header.substr(pos + 2);
+			if (key.find_first_of(" \t\n\r") != std::string::npos)
+				error 400;
+			if (header[pos+1] == ' ')
+				this->headers[key] = header.substr(pos + 2);
+			else
+				this->headers[key] = header.substr(pos + 1);
+		}
+		else if (pos = header.find_first_not_of(" \t") != std::string::npos) //헤더가 여러줄로 나뉘어 있으면
+		{
+			this->headers[key] += header.substr(pos);
 		}
 		else
 			//error?

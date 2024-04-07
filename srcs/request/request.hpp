054c004b891361b70b2515fd208ac88f939861e9
vivivim <130360809+vivivim@users.noreply.github.com>
2024-04-07 20:59:23 +0900

    에러 처리 중. . .

--- srcs/request/request.hpp
+++ srcs/request/request.hpp
@@ -1,13 +1,16 @@
 #ifndef REQUEST_HPP
 # define REQUEST_HPP
 
+#include <string>
+#include <unordered_map>
+
 class	request : public cahn {
 	private:
 		std::string							method;
 		std::string							path;
 		std::string							version;
-		std::map<std::string, std::string>	headers;
-		int									is_body;
+		std::unordered_map<std::string, std::string>	headers;
+		int									isBody;
 		std::string							body;
 
 	public:
